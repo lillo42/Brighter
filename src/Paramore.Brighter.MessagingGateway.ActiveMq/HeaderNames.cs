ef1e373cfd92c22b86a2298d709b24aa87b14570
Rafael Andrade <rafael.andrade@justeattakeaway.com>
2025-07-31 10:46:48 +0100

    feat: Add missing file

---
+++ src/Paramore.Brighter.MessagingGateway.ActiveMq/HeaderNames.cs
@@ -0,0 +1,13 @@
+﻿namespace Paramore.Brighter.MessagingGateway.ActiveMq;
+
+public class HeaderNames
+{
+    public const string DataContentType = "datacontenttype";
+    public const string DataSchema = "dataschema";
+    public const string MessageType = "messagetype";
+    public const string SpecVersion = "specversion";
+    public const string Source = "source";
+    public const string Subject = "subject";
+    public const string Topic = "topic";
+    
+}
